7647b4fb54f1f1583e47523562e14b9f48087cf6
Irfan Ammar Afif <ipangcheater@gmail.com>
2023-09-20 08:03:01 +0700

    entrypoint: bypass gosu setuid restriction

--- imagefiles/entrypoint.sh
+++ imagefiles/entrypoint.sh
@@ -40,6 +40,9 @@ if [[ -n $BUILDER_UID ]] && [[ -n $BUILDER_GID ]]; then
     chown root:$BUILDER_GID "$(which gosu)"
     chmod +s "$(which gosu)"; sync
 
+    # Bypass gosu setuid restriction
+    export GOSU_PLEASE_LET_ME_BE_COMPLETELY_INSECURE_I_GET_TO_KEEP_ALL_THE_PIECES="I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhäuser Gate. All those moments will be lost in time, like tears in rain. Time to die."
+
     # Execute project specific pre execution hook
     if [[ -e /work/.dockcross ]]; then
        gosu $BUILDER_UID:$BUILDER_GID /work/.dockcross
