b1f84bdeacd98eeb6654f1d3d038ef0366e1ae67
Irfan Ammar Afif <ipangcheater@gmail.com>
2023-09-20 08:09:20 +0700

    linux-arm64: update
    - use crosstool-NG 1.26.0_rc2
    - upgrade gcc version to 13.2.0
    - downgrade glibc to 1.31 for better compatibility
    - update companion libraries
    - update environment
    - update cmake toolchain file to follow environment

--- linux-arm64/Toolchain.cmake
+++ linux-arm64/Toolchain.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME $ENV{OS})
 set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_SYSTEM_PROCESSOR $ENV{ARCH})
 
-set(cross_triple "aarch64-unknown-linux-gnu")
-set(cross_root /usr/xcc/${cross_triple})
+set(cross_triple $ENV{CROSS_TRIPLE})
+set(cross_root $ENV{CROSS_ROOT})
 
 set(CMAKE_C_COMPILER $ENV{CC})
 set(CMAKE_CXX_COMPILER $ENV{CXX})
@@ -17,4 +17,4 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
 
-set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-${CMAKE_SYSTEM_PROCESSOR})
